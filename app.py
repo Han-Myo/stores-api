d80eb0a439d86b24416904e4c87d1b1347203d6e
Han-Myo <ibifuromoon@gmail.com>
2022-01-31 11:26:14 +0100

    Midified postgres

--- app.py
+++ app.py
@@ -14,10 +14,10 @@ from db import db
     
 
 app = Flask(__name__)
-#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 uri = os.getenv("DATABASE_URL", 'sqlite:///data.db')  # or other relevant config var
 if uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = "hanmyo"
 api = Api(app)
